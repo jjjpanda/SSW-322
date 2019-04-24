c79ed5c03c84a869811fa98facd5adff72e921e9
zildj <zildj100@gmail.com>
2019-04-23 22:41:56 -0400

    myTests beginings

---
+++ public/res/myTests.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    listTestsSurveys(localStorage)
+}
+
+function listTestsSurveys() {
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+        console.log(i)
+        console.log(localStorage.key(i))
+        console.log(localStorage.getItem(localStorage.key(i)))
+        if (localStorage.key(i) == "displayTestName" || !localStorage.getItem(localStorage.key(i)).startsWith("{")) {
+            ""
+        }
+        else if (JSON.parse(localStorage.getItem(localStorage.key(i)))["type"] == "test") {
+            writeToDiv(format(localStorage.key(i)), "tests")
+        }
+        else if (JSON.parse(localStorage.getItem(localStorage.key(i)))["type"] == "survey") {
+            writeToDiv(format(localStorage.key(i)), "surveys")
+        }
+    }
+}
+
+function format(title) {
+    return '<button class="button" onclick="showTest(\''+title+'\')">'+title+'</button>'
+}
+
+function showTest(testName) {
+    localStorage["displayTestName"] = testName
+    window.location.href ='displayTest.html'
+}
+
+function writeToDiv(text, div) {
+    output = document.getElementById(div);
+    if (output.innerHTML == "You have no tests")
+        output.innerHTML = "<h1>Test List</h1>"
+    if (output.innerHTML == "You have no surveys")
+        output.innerHTML = "<h1>Survey List</h1>"
+    var pre = document.createElement("p");
+	pre.innerHTML = text;
+    output.appendChild(pre);
+}
